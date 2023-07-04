bf8d3a97a562c98b42cf8d0e7c48474283fe50cf
fahadtufail007 <fahadtufail873@gmail.com>
2023-07-04 06:35:24 +0500

    login functionality

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,40 @@
-import React from "react";
-
-import styles from "../styles/Login.module.css";
-import { Button, TextInput } from "../components";
-import calderCapitalLogo from "../assets/svgs/calder-capital-logo.svg";
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { loginAction } from "../store/thunk/login.thunk";
+import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Button, TextInput } from "../components";
+import styles from "../styles/Login.module.css";
+import calderCapitalLogo from "../assets/svgs/calder-capital-logo.svg";
+import { login } from "../store/reducers/login.reducer";
 
 const Login = () => {
+
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const [password, setPassword] = useState("")
   const [email, setEmail] = useState("")
+  const loading = useSelector((state) => state.auth.loading);
+  const error = useSelector((state) => state.auth.error);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  // const currState = useSelector((state) => state.auth);
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const isButtonDisabled = !email || !password || !email.match(emailRegex);
+  
+  const handleLogin = () => {
+    console.log("hi baby")
+    debugger;
+    dispatch(login( {email, password}))
+  }
+
+  if (isLoggedIn) {
+    // if (currState.token.role == "admin") {
+    //   console.log("currState", currState);
+      navigate("/clients");
+    // }  
+  }
+
   return (
     <div className="row">
       <div className={`col-6 ${styles.leftSide}`}>
@@ -39,7 +57,10 @@ const Login = () => {
               id="flexCheckDefault"
             />
           </div>
-            <Button title="Login" onClick={()=> dispatch(loginAction({value: {email, password}, navigate}))} disabled={isButtonDisabled}/>
+            <Button title="Login" onClick={() => handleLogin} disabled={loading}>
+              {loading ? 'Loading...' : 'Login'}
+            </Button>
+            {error && <p>{error}</p>}
         </div>
       </div>
     </div>
